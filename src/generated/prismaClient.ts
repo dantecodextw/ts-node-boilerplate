21c63d66ba5ff9d5ffee39a2587e7af76096caff
dantecodextw <anshulrajput@thoughtwin.com>
2025-05-01 20:07:43 +0530

    Add Prisma setup and update dependencies for database integration

---
+++ src/generated/prismaClient.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+  omit: { user: { password: true } },
+});
+
+export default prisma;
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,11 @@
+import prisma from '../generated/prismaClient';
 import { SignupData } from '../types/auth.type';
 
-const signup = async (validatedData: SignupData) => {};
+const signup = async (validatedData: SignupData) => {
+  // const user = await prisma.user.create({
+  //   // data: {}
+  // })
+};
 
 export default {
   signup,
